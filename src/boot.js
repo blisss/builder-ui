d7d4178fdae4953b7c3f28c8f3caaacda5145e42
yuffiy <yfhj1990@hotmail.com>
2017-07-22 19:16:41 +0800

    Init commit.

---
+++ src/boot.js
@@ -0,0 +1,14 @@
+// -*- mode: js-jsx -*-
+// -*- coding: utf-8 -*-
+// @flow
+// @jsx h
+
+import { h, render } from 'ink'
+import Root from './index'
+
+function main(): void {
+  render(<Root />)
+}
+
+main()
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// -*- mode: js-jsx -*-
+// -*- coding: utf-8 -*-
+// @flow
+// @jsx h
+
+import { h, render, Component, Text } from 'ink'
+
+class Builder extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+	  return <Text green>Hello world</Text>
+  }
+}
+
+export default Builder
